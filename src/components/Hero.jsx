87d9fe824658632732d165f35b0087851bbcff17
Juan Urdaneta <juandurdanet@gmail.com>
2025-04-03 15:27:52 -0400

    enhance Hero component with state management and video handling; update App layout styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVidRef = useRef(null);
+
+  const handleMiniVidClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div>MiniVideoPlayer</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
