45af186fc0c49f8dadcc3c9421f9870fe83d7a28
Juan Urdaneta <juandurdanet@gmail.com>
2025-04-03 16:25:48 -0400

    refactor Hero component to improve video handling and update video indexing logic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,21 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 4;
-  const nextVidRef = useRef(null);
+  const totalVideos = 3;
+  const nextVideoRef = useRef(null);
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
   const handleMiniVidClick = () => {
     setHasClicked(true);
 
-    setCurrentIndex((prevIndex) => prevIndex + 1);
+    setCurrentIndex(upcomingVideoIndex);
+  };
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
   };
 
   return (
@@ -23,8 +31,40 @@ const Hero = () => {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div>MiniVideoPlayer</div>
+            <div
+              onClick={handleMiniVidClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVideoRef}
+                src={getVideoSrc(upcomingVideoIndex)}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
           </div>
+          <video
+            ref={nextVideoRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover object-center"
+          />
         </div>
       </div>
     </div>
