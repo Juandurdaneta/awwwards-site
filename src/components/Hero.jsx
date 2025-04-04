43972ae5f448b43e5594b40bfe502e3bf4bde6da
Juandurdaneta <juandurdanet@gmail.com>
2025-04-03 21:54:21 -0400

    add Button component and integrate with Hero; update package.json for react-icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -61,7 +63,6 @@ const Hero = () => {
             src={getVideoSrc(
               currentIndex === totalVideos - 1 ? 1 : currentIndex
             )}
-            autoPlay
             loop
             muted
             className="absolute left-0 top-0 size-full object-cover object-center"
@@ -73,7 +74,27 @@ const Hero = () => {
           G<b>A</b>MING
         </h1>
 
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              Redefi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the metagame Layer <br />
+              Unleash the Play Economy
+            </p>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="bg-yellow-300 flex flex-center gap-1"
+            />
+          </div>
+        </div>
       </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>A</b>MING
+      </h1>
     </div>
   );
 };
